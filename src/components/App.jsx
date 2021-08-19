d7949f719cf909ae713f87e1ed14a0ecb30cd3ef
KevinVillegasDev <kevin.villegas94@gmail.com>
2021-08-19 12:34:25 -0700

    Created react youtube player

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,22 @@
 import React, {Component} from 'react';
+import ReactPlayer from 'react-player';
 
 class App extends Component {
     constructor(props) {
         super(props);
-        this.state = {  }
+        this.state = { 
+
+         }
     }
+    
+    
+    
+    
     render() { 
         return ( 
-            <h1> naruto </h1>
+            <div>
+                <ReactPlayer url='https://www.youtube.com/watch?v=ysz5S6PUM-U' />
+            </div> 
          );
     }
 }
