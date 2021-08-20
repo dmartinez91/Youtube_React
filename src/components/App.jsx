9c6af52de352018202f00b045e8ac6a10c91a03b
dmartinez91 <dmartinez3691@gmail.com>
2021-08-20 10:57:56 -0400

    refactored embeded video to display on our page using iframe

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,29 @@
 import React, {Component} from 'react';
 import ReactPlayer from 'react-player';
 import axios from 'axios';
-import Youtube from './Youtube/Youtube'
+
 
 class App extends Component {
     constructor(props) {
         super(props);
         this.state = { 
-
+            displayVideo: []
          }
     }
     
+
+    async makeGetRequest(){
+        try{
+            let response = await axios.get('https://www.googleapis.com/youtube/v3/search?q=dogsgsp&key=AIzaSyDPP_0jwJ7O2lH6pJKVBbNwqrs28QmH0lo&part=snippet')
+            console.log(this)
+            this.setState({
+                displayVideo: response.data
+            });
+        }
+        catch(ex){
+            console.log(ex)
+        }
+    }
 
     
     
@@ -18,7 +31,12 @@ class App extends Component {
         return ( 
 
             <div>
-                {Youtube}
+                <h1> Youtube Clone </h1>
+                
+                <iframe title='youtube-video' id="ytplayer" type="text/html" width="640" height="360"
+                src={`https://www.youtube.com/embed/${this.state.displayVideo}?autoplay=1&origin=http://example.com`}
+                frameborder="0"></iframe>
+                
             </div> 
 
          );
